e0827f73b1f4d45496c324bf574bf141c112ad5f
Tom W Hartung <tomwhartung@gmail.com>
2015-12-17 14:06:16 -0700

    Made updates to ConfigForm.php that are needed to make this the new my_hugs module.

--- modules/jmws/my_hugs/src/Form/ConfigForm.php
+++ modules/jmws/my_hugs/src/Form/ConfigForm.php
@@ -1,21 +1,21 @@
 <?php
 
-namespace Drupal\hugs\Form;
+namespace Drupal\my_hugs\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 class ConfigForm extends ConfigFormBase {
   public function getFormId() {
-    return 'hug_config';
+    return 'my_hug_config';
   }
 
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('hugs.settings');
+    $config = $this->config('my_hugs.settings');
 
     $form['default_count'] = [
       '#type' => 'number',
-      '#title' => $this->t('Default hug count'),
+      '#title' => $this->t('Default my_hug count'),
       '#default_value' => $config->get('default_count'),
     ];
 
@@ -25,12 +25,12 @@ class ConfigForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
 
-    $config = $this->config('hugs.settings');
+    $config = $this->config('my_hugs.settings');
     $config->set('default_count', $form_state->getValue('default_count'));
     $config->save();
   }
 
   public function getEditableConfigNames() {
-    return ['hugs.settings'];
+    return ['my_hugs.settings'];
   }
 }
