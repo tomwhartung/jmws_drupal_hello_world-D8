61910149c15c6c9faeae3982c8c1979e0462878a
Tom W Hartung <tomwhartung@gmail.com>
2015-12-25 16:26:56 -0700

    Got TeachMe experiment to work in Plugin/Block/MyHugStatus.php .

--- modules/jmws/my_hugs/src/Plugin/Block/MyHugStatus.php
+++ modules/jmws/my_hugs/src/Plugin/Block/MyHugStatus.php
@@ -8,6 +8,14 @@ use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\my_hugs\MyHugTracker;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+//
+// Using this code to help understand how the namespaces, use statements, and autoloading all work together
+// For details, see the comments in the build method.
+//
+use Drupal\my_hugs\TeachMe;         // (0)
+use Drupal\my_hugs\LearningMore;   // (1)
+// use Drupal\my_hugs\LearningMore\LearningMore;  // (2)
+use Drupal\my_hugs\JmwsIdMyGadget;
 
 /**
  * Reports on myHugability status.
@@ -64,6 +72,49 @@ class MyHugStatus extends BlockBase implements ContainerFactoryPluginInterface {
     else {
       $message = $this->t('Srsly wtf, no my_hugs :-(');
     }
+
+    // (0) 
+    $teachMe = new TeachMe( 'MyHugStatus::build()' );
+
+	// $jmwsIdMyGadget = new \Drupal\idmygadget\JmwsIdMyGadget\JmwsIdMyGadgetDrupal();
+	// $jmwsIdMyGadget = new Drupal\idmygadget\JmwsIdMyGadget\JmwsIdMyGadgetDrupal();
+	// $jmwsIdMyGadget = new JmwsIdMyGadgetDrupal();
+	//
+	// (1) and (2) Does NOT work in either case, it looks for the class inside of this namespace
+	// ie: "Class 'Drupal\\idmygadget\\Plugin\\Block\\Drupal\\idmygadget\\LearningMore\\LearningMore' not found"
+	// $learningMore = new Drupal\idmygadget\LearningMore\LearningMore( 'IdMyGadgetStatus::build()' );
+	// 
+	// (1) Works when we "use Drupal\idmygadget\LearningMore;"
+	// (2)            OR "use Drupal\idmygadget\LearningMore\LearningMore;"
+	// $learningMore_1 = new \Drupal\idmygadget\LearningMore\LearningMore( 'IdMyGadgetStatus::build() - 1' );
+	//
+	// (1) Does NOT work when we ""
+	// ie: "Class 'Drupal\\idmygadget\\LearningMore' not found..."
+	// (2) Works when we "use Drupal\idmygadget\LearningMore\LearningMore;"
+	// $learningMore_2 = new LearningMore( 'IdMyGadgetStatus::build() - 2' );
+
+	if ( class_exists('TeachMe') ) {
+		$message .= '<br />TeachMe is a class!';
+	}
+	else {
+		$message .= '<br />Oops TeachMe is a NOT class.';
+	}
+
+	if ( class_exists('BlockBase') ) {
+		$message .= '<br />BlockBase is a class!';
+	}
+	else {
+		$message .= '<br />Oops BlockBase is a NOT class.';
+	}
+
+	if ( class_exists('MyHugStatus') ) {
+		$message .= '<br />MyHugStatus is a class!';
+	}
+	else {
+		$message .= '<br />Oops MyHugStatus is a NOT class.';
+	}
+
+    
     return [
       '#markup' => $message,
     ];
