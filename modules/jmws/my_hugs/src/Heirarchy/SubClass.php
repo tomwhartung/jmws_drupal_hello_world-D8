8ab7cc54710690884f19c43e64b949a49d620904
Tom W Hartung <tomwhartung@gmail.com>
2015-12-26 15:28:49 -0700

    Added minimal essential comments and brought code in the Heirarchy/* classes up to standards.

--- modules/jmws/my_hugs/src/Heirarchy/SubClass.php
+++ modules/jmws/my_hugs/src/Heirarchy/SubClass.php
@@ -1,12 +1,22 @@
 <?php
 namespace Drupal\my_hugs\Heirarchy;
 
+/**
+ * Trivial subclass for testing using namespaces with inheritance
+ */
 class SubClass extends TheBase {
-	public function __construct( $createdBy='' ) {
-		error_log( 'This SubClass object was created by ' . $createdBy . '.' );
-	}
 
-	public function logToday() {
-		error_log( 'SubClass class is logging today.' );
-	}
+  /**
+   * Constructor in trivial class does something trivial yet very important 
+   */
+  public function __construct( $createdBy='' ) {
+    error_log( 'This SubClass object was created by ' . $createdBy . '.' );
+  }
+
+  /**
+   * Unused (so far) function for our trivial class
+   */
+  public function logToday() {
+    error_log( 'SubClass class is logging today.' );
+  }
 }
--- modules/jmws/my_hugs/src/Heirarchy/TheBase.php
+++ modules/jmws/my_hugs/src/Heirarchy/TheBase.php
@@ -1,12 +1,22 @@
 <?php
 namespace Drupal\my_hugs\Heirarchy;
 
+/**
+ * Trivial base class for testing using namespaces with inheritance
+ */
 class TheBase {
-	public function __construct( $createdBy='' ) {
-		error_log( 'This TheBase object was created by ' . $createdBy . '.' );
-	}
 
-	public function logToday() {
-		error_log( 'TheBase is logging today.' );
-	}
+  /**
+   * Constructor in trivial class does something trivial yet very important 
+   */
+  public function __construct( $createdBy='' ) {
+    error_log( 'This TheBase object was created by ' . $createdBy . '.' );
+  }
+
+  /**
+   * Unused (so far) function for our trivial class
+   */
+  public function logToday() {
+    error_log( 'TheBase is logging today.' );
+  }
 }
