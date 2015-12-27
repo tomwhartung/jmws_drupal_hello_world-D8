661530cbcff5167e78ce69d15008ef20bb4012b5
Tom W Hartung <tomwhartung@gmail.com>
2015-12-26 17:08:19 -0700

    Updated logToday in GlobalServiceDrupal.php to print an optional message.

--- modules/jmws/my_hugs/src/GoGlobal/GlobalServiceDrupal.php
+++ modules/jmws/my_hugs/src/GoGlobal/GlobalServiceDrupal.php
@@ -1,14 +1,23 @@
 <?php
 namespace {
-  require GlobalService.php;
+  require 'GlobalService.php';
 
   class GlobalServiceDrupal extends GlobalService {
     public function __construct( $createdBy='' ) {
       error_log( 'This GlobalServiceDrupal object was created by ' . $createdBy . '.' );
     }
 
-    public function logToday() {
-      error_log( 'GlobalServiceDrupal class is logging today.' );
+    /**
+     * Trivial function for our trivial class
+     * I think we will actually use this one to verify everything works!
+     */
+    public function logToday( $message='' ) {
+      if ( $message == '' ) {
+        error_log( 'GlobalServiceDrupal class is logging today.' );
+      } else {
+        error_log( 'GlobalServiceDrupal message is: ' . $message );
+      }
+      parent::logToday();
     }
   }
 }
