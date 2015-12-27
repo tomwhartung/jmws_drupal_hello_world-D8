a4428d8b9dcc21d58d83c0a3c6cd1b4a76d8c8ae
Tom W Hartung <tomwhartung@gmail.com>
2015-12-26 17:16:29 -0700

    Updated the non-namespaced part of NamespacedService.php to create the subclass object (GlobalServiceDrupal) and make it global.

--- modules/jmws/my_hugs/src/GoGlobal/NamespacedService.php
+++ modules/jmws/my_hugs/src/GoGlobal/NamespacedService.php
@@ -6,15 +6,22 @@ namespace Drupal\my_hugs\GoGlobal {
       error_log( 'This NamespacedService object was created by ' . $createdBy . '.' );
     }
 
+    /**
+     * Unused (so far) function for our trivial class
+     */
     public function logToday() {
       error_log( 'NamespacedService is logging today.' );
     }
   }
 }
-
+//
+// THis code is in the global namespace
+//
 namespace {
-  // require_once 'GlobalServiceDrupal.php';
-  require_once 'GlobalService.php';
+  // require_once 'GlobalService.php';
+  // $globalServiceObject = new GlobalService( 'non-namespaced area in NamespacedService.php' );
 
-  $globalServiceObject = new GlobalService( 'non-namespaced area in NamespacedService.php' );
+  global $globalServiceObject;
+  require_once 'GlobalServiceDrupal.php';
+  $globalServiceObject = new GlobalServiceDrupal( 'non-namespaced area in NamespacedService.php' );
 }
